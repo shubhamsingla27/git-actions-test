104a4ebfaad25ceeb6a06744fe4d8e379c000aac
Shubham Singla <shubhamsingla@c02z5dywlvdc.attlocal.net>
2022-07-01 09:46:01 -0700

    test coverage reduced for testing

--- src/App.test.js
+++ src/App.test.js
@@ -61,64 +61,64 @@ test("show error on invalid email", () => {
   expect(emailErrorElementAgain).toBeInTheDocument();
 });
 
-test("if password is less than 5 characters, show error", () => {
-  render(<App />);
+// test("if password is less than 5 characters, show error", () => {
+//   render(<App />);
 
-  const passwordErrorElement = screen.queryByText(
-    /The password should contain 5 or more characters/i
-  );
+//   const passwordErrorElement = screen.queryByText(
+//     /The password should contain 5 or more characters/i
+//   );
 
-  const emailInputElement = screen.getByRole("textbox", {
-    name: /email/i,
-  });
+//   const emailInputElement = screen.getByRole("textbox", {
+//     name: /email/i,
+//   });
 
-  const passwordInputElement = screen.getByLabelText("Password");
+//   const passwordInputElement = screen.getByLabelText("Password");
 
-  const submitButtonElement = screen.getByRole("button", {
-    name: /submit/i,
-  });
+//   const submitButtonElement = screen.getByRole("button", {
+//     name: /submit/i,
+//   });
 
-  userEvent.type(emailInputElement, "selena@test.com");
-  expect(passwordErrorElement).not.toBeInTheDocument();
-  userEvent.type(passwordInputElement, "123");
-  userEvent.click(submitButtonElement);
+//   userEvent.type(emailInputElement, "selena@test.com");
+//   expect(passwordErrorElement).not.toBeInTheDocument();
+//   userEvent.type(passwordInputElement, "123");
+//   userEvent.click(submitButtonElement);
 
-  const passwordErrorElementAgain = screen.queryByText(
-    /The password should contain 5 or more characters/i
-  );
-  expect(passwordErrorElementAgain).toBeInTheDocument();
-});
+//   const passwordErrorElementAgain = screen.queryByText(
+//     /The password should contain 5 or more characters/i
+//   );
+//   expect(passwordErrorElementAgain).toBeInTheDocument();
+// });
 
-test("show error if passwords dont match", () => {
-  render(<App />);
+// test("show error if passwords dont match", () => {
+//   render(<App />);
 
-  const confirmPasswordErrorElement = screen.queryByText(
-    /the passwords dont match. try again/i
-  );
+//   const confirmPasswordErrorElement = screen.queryByText(
+//     /the passwords dont match. try again/i
+//   );
 
-  const emailInputElement = screen.getByRole("textbox", {
-    name: /email/i,
-  });
+//   const emailInputElement = screen.getByRole("textbox", {
+//     name: /email/i,
+//   });
 
-  const passwordInputElement = screen.getByLabelText("Password");
-  const confirmPasswordInputElement =
-    screen.getByLabelText(/confirm password/i);
+//   const passwordInputElement = screen.getByLabelText("Password");
+//   const confirmPasswordInputElement =
+//     screen.getByLabelText(/confirm password/i);
 
-  const submitButtonElement = screen.getByRole("button", {
-    name: /submit/i,
-  });
+//   const submitButtonElement = screen.getByRole("button", {
+//     name: /submit/i,
+//   });
 
-  userEvent.type(emailInputElement, "selena@test.com");
-  userEvent.type(passwordInputElement, "12345");
-  expect(confirmPasswordErrorElement).not.toBeInTheDocument();
-  userEvent.type(confirmPasswordInputElement, "12346");
-  userEvent.click(submitButtonElement);
+//   userEvent.type(emailInputElement, "selena@test.com");
+//   userEvent.type(passwordInputElement, "12345");
+//   expect(confirmPasswordErrorElement).not.toBeInTheDocument();
+//   userEvent.type(confirmPasswordInputElement, "12346");
+//   userEvent.click(submitButtonElement);
 
-  const confirmPasswordErrorElementAgain = screen.queryByText(
-    /the passwords dont match. try again/i
-  );
-  expect(confirmPasswordErrorElementAgain).toBeInTheDocument();
-});
+//   const confirmPasswordErrorElementAgain = screen.queryByText(
+//     /the passwords dont match. try again/i
+//   );
+//   expect(confirmPasswordErrorElementAgain).toBeInTheDocument();
+// });
 
 test("should show no error if every input is valid", () => {
   render(<App />);
